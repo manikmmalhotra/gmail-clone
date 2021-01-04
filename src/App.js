175f8a621b93784c5a6f4c56af01bec5b646f6bb
SEJAL GOYAL <sejalgoyal16@gmail.com>
2021-01-04 13:53:08 +0530

    composebox

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Header from './Header.js';
 import Sidebar from './Sidebar.js';
 import Mail from './Mail.js';
 import EmailList from './EmailList.js';
+import SendMail from './SendMail.js';
+import {useSelector} from 'react-redux';
+import selectSendMessageIsOpen from './features/mailSlice';
 import './App.css';
-// import { Router } from '@material-ui/icons';
-import {  BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {  BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
+  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen)
   return (
     <Router>
       <div className="app">
@@ -26,6 +29,7 @@ function App() {
         </Route>
       </Switch>
       </div>
+      {/* {sendMessageIsOpen && <SendMail />} */}
     </div> 
     </Router>
   );
