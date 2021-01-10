3d1bd5e9588a0da58568d0c9e5d1e6fa1722d1fd
manikmmalhotra <manikmalhotra2113@gmail.com>
2021-01-10 18:12:52 +0530

    final

--- src/Mail.js
+++ src/Mail.js
@@ -1,80 +1,83 @@
 import { IconButton } from "@material-ui/core";
 import React from "react";
 import "./Mail.css";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
-import ErrorIcon from "@material-ui/icons/Error";
-import DeleteIcon from "@material-ui/icons/Delete";
-import EmailIcon from "@material-ui/icons/Email";
-import WatchLaterIcon from "@material-ui/icons/WatchLater";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import LabelImportantIcon from "@material-ui/icons/LabelImportant";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
-import PrintIcon from "@material-ui/icons/Print";
-import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import ArrowBackRoundedIcon from "@material-ui/icons/ArrowBackRounded";
+import MoveToInboxRoundedIcon from "@material-ui/icons/MoveToInboxRounded";
+import ErrorRoundedIcon from "@material-ui/icons/ErrorRounded";
+import DeleteRoundedIcon from "@material-ui/icons/DeleteRounded";
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import WatchLaterRoundedIcon from "@material-ui/icons/WatchLaterRounded";
+import CheckCircleRoundedIcon from "@material-ui/icons/CheckCircleRounded";
+import LabelImportantRoundedIcon from "@material-ui/icons/LabelImportantRounded";
+import MoreVertRoundedIcon from "@material-ui/icons/MoreVertRounded";
 import { useHistory } from "react-router-dom";
+import UnfoldMoreRoundedIcon from "@material-ui/icons/UnfoldMoreRounded";
+import PrintRoundedIcon from "@material-ui/icons/PrintRounded";
+import ExitToAppRoundedIcon from "@material-ui/icons/ExitToAppRounded";
+import { useSelector } from "react-redux";
+import { selectOpenMail } from "./features/mailSlice";
 
-function Mail() {
+const Mail = () => {
   const history = useHistory();
+  const selectedMail = useSelector(selectOpenMail);
+
   return (
     <div className="mail">
       <div className="mail__tools">
         <div className="mail__toolsLeft">
           <IconButton onClick={() => history.push("/")}>
-            <ArrowBackIcon />
+            <ArrowBackRoundedIcon />
           </IconButton>
           <IconButton>
-            <MoveToInboxIcon />
+            <MoveToInboxRoundedIcon />
           </IconButton>
           <IconButton>
-            <ErrorIcon />
+            <ErrorRoundedIcon />
           </IconButton>
           <IconButton>
-            <DeleteIcon />
+            <DeleteRoundedIcon />
           </IconButton>
           <IconButton>
-            <EmailIcon />
+            <EmailRoundedIcon />
           </IconButton>
           <IconButton>
-            <WatchLaterIcon />
+            <WatchLaterRoundedIcon />
           </IconButton>
           <IconButton>
-            <CheckCircleIcon />
+            <CheckCircleRoundedIcon />
           </IconButton>
           <IconButton>
-            <LabelImportantIcon />
+            <LabelImportantRoundedIcon />
           </IconButton>
           <IconButton>
-            <MoreVertIcon />
+            <MoreVertRoundedIcon />
           </IconButton>
         </div>
-
         <div className="mail__toolsRight">
           <IconButton>
-            <UnfoldMoreIcon />
+            <UnfoldMoreRoundedIcon />
           </IconButton>
           <IconButton>
-            <PrintIcon />
+            <PrintRoundedIcon />
           </IconButton>
           <IconButton>
-            <ExitToAppIcon />
+            <ExitToAppRoundedIcon />
           </IconButton>
         </div>
       </div>
       <div className="mail__body">
         <div className="mail__bodyHeader">
-          <h2>Subject</h2>
-          <LabelImportantIcon className="mail__important" />
-          <p>Title</p>
-          <p className="mail__time">10pm</p>
+          <h2>{selectedMail?.subject}</h2>
+          <LabelImportantRoundedIcon className="mail__important" />
+          <p>{selectedMail?.title}</p>
+          <p className="mail__time">{selectedMail?.time}</p>
         </div>
         <div className="mail__message">
-          <p>hEY! THERE</p>
+          <p>{selectedMail?.description}</p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Mail;
